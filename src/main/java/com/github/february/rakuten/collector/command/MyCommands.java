960394498c136f24ebb3f95ce1f46b4aa58aaaa7
zhanghao <zhanghao@shoujins.com>
2018-12-06 19:20:04 +0800

    调用乐天服务

--- src/main/java/com/github/february/rakuten/collector/command/MyCommands.java
+++ src/main/java/com/github/february/rakuten/collector/command/MyCommands.java
@@ -1,9 +1,13 @@
 package com.github.february.rakuten.collector.command;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.february.rakuten.collector.bean.AvailableShoeSize;
 import com.github.february.rakuten.collector.service.AnalyzeService;
 import com.github.february.rakuten.collector.service.BrowserService;
@@ -22,7 +26,7 @@ public class MyCommands {
 	RakutenService rakutenService;
 	
 	@ShellMethod("Add two integers together.")
-    public int dec(int a, int b) {
+    public int dec(int a, int b) throws JsonParseException, JsonMappingException, IOException {
 		rakutenService.ichibaItemSearch();
 		return a-b;
 	}
--- src/main/java/com/github/february/rakuten/collector/service/RakutenService.java
+++ src/main/java/com/github/february/rakuten/collector/service/RakutenService.java
@@ -1,5 +1,6 @@
 package com.github.february.rakuten.collector.service;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,19 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.february.rakuten.collector.bean.IchibaItemSearchResult;
 import com.github.february.rakuten.collector.config.RakutenConfig;
 
 @Component
 public class RakutenService {
-	
+
 	@Autowired
 	RakutenConfig rakutenConfig;
 
 	@Autowired
 	RestTemplate restTemplate;
-	
-	public void ichibaItemSearch() {
+
+	public void ichibaItemSearch() throws JsonParseException, JsonMappingException, IOException {
 		String url = rakutenConfig.getUrl().getIchibaItem();
 		Map<String, String> uriVariables = new HashMap<String, String>();
 		uriVariables.put("applicationId", rakutenConfig.getApplicationId());
@@ -27,7 +30,10 @@ public class RakutenService {
 		uriVariables.put("format", rakutenConfig.getFormat());
 		uriVariables.put("formatVersion", rakutenConfig.getFormatVersion());
 		uriVariables.put("keyword", "addidas");
-		restTemplate.postForObject(url, uriVariables, IchibaItemSearchResult.class);
+
+		IchibaItemSearchResult result = restTemplate.postForObject(url, uriVariables, IchibaItemSearchResult.class);
+
+		System.out.println(result.getCount());
 	}
 
 }
